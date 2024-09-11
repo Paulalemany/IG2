8248f2cf915e6e7831557daa675ed5db60fbfb43
Paulalemany <palemany@ucm.es>
2024-09-11 11:17:52 +0200

    Proyecto de Ogre

---
+++ ProyectosOGREvc15x86/ProyectosOGREvc15x86/IG2App/IG2App.cpp
@@ -0,0 +1,111 @@
+#include "IG2App.h"
+
+using namespace Ogre;
+using namespace std;
+
+bool IG2App::keyPressed(const OgreBites::KeyboardEvent& evt){
+        
+    // ESC key finished the rendering...
+    if (evt.keysym.sym == SDLK_ESCAPE){
+        getRoot()->queueEndRendering();
+    }
+    
+  return true;
+}
+
+void IG2App::shutdown(){
+    
+  mShaderGenerator->removeSceneManager(mSM);
+  mSM->removeRenderQueueListener(mOverlaySystem);  
+					
+  mRoot->destroySceneManager(mSM);  
+
+  delete mTrayMgr;  mTrayMgr = nullptr;
+  delete mCamMgr; mCamMgr = nullptr;
+  
+  // do not forget to call the base 
+  IG2ApplicationContext::shutdown(); 
+}
+
+void IG2App::setup(void){
+    
+    // do not forget to call the base first
+    IG2ApplicationContext::setup();
+
+    // Create the scene manager
+    mSM = mRoot->createSceneManager();
+
+    // Register our scene with the RTSS
+    mShaderGenerator->addSceneManager(mSM);
+
+    mSM->addRenderQueueListener(mOverlaySystem);
+    mTrayMgr = new OgreBites::TrayManager("TrayGUISystem", mWindow.render);
+    mTrayMgr->showFrameStats(OgreBites::TL_BOTTOMLEFT);
+    addInputListener(mTrayMgr);
+    
+    // Adds the listener for this object
+    addInputListener(this);
+    setupScene();
+}
+
+void IG2App::setupScene(void){
+    
+    //------------------------------------------------------------------------
+    // Creating the camera
+    
+    Camera* cam = mSM->createCamera("Cam");
+    cam->setNearClipDistance(1);
+    cam->setFarClipDistance(10000);
+    cam->setAutoAspectRatio(true);
+    //cam->setPolygonMode(Ogre::PM_WIREFRAME);
+            
+    mCamNode = mSM->getRootSceneNode()->createChildSceneNode("nCam");
+    mCamNode->attachObject(cam);
+
+    mCamNode->setPosition(0, 0, 1000);
+    mCamNode->lookAt(Ogre::Vector3(0, 0, 0), Ogre::Node::TS_WORLD);
+    
+    // and tell it to render into the main window
+    Viewport* vp = getRenderWindow()->addViewport(cam);
+    
+    mCamMgr = new OgreBites::CameraMan(mCamNode);
+    addInputListener(mCamMgr);
+    mCamMgr->setStyle(OgreBites::CS_ORBIT);
+    
+    
+    //------------------------------------------------------------------------
+    // Creating the light
+    
+    //mSM->setAmbientLight(ColourValue(0.5, 0.5, 0.5));
+    Light* luz = mSM->createLight("Luz");
+    luz->setType(Ogre::Light::LT_DIRECTIONAL);
+    luz->setDiffuseColour(0.75, 0.75, 0.75);
+
+    mLightNode = mSM->getRootSceneNode()->createChildSceneNode("nLuz");
+    //mLightNode = mCamNode->createChildSceneNode("nLuz");
+    mLightNode->attachObject(luz);
+    mLightNode->setDirection(Ogre::Vector3(0, 0, -1));
+    
+
+    
+    //------------------------------------------------------------------------
+    // Creating Sinbad
+
+    Ogre::Entity* ent = mSM->createEntity("Sinbad.mesh");
+    mSinbadNode = mSM->getRootSceneNode()->createChildSceneNode("nSinbad");
+    mSinbadNode->attachObject(ent);
+
+    // Show bounding box
+    mSinbadNode->showBoundingBox(true);
+    
+    // Set position of Sinbad
+    //mSinbadNode->setPosition(x, y, z);
+    
+    // Set scale of Sinbad
+    mSinbadNode->setScale(20, 20, 20);
+    
+    //mSinbadNode->yaw(Ogre::Degree(-45));
+    //mSinbadNode->setVisible(false);    
+}
+
+
---
+++ ProyectosOGREvc15x86/ProyectosOGREvc15x86/IG2App/IG2Main.cpp
@@ -0,0 +1,18 @@
+
+#include "IG2App.h"
+
+int main(int argc, char *argv[])
+{
+    IG2App app;
+    
+  try {
+    srand((unsigned)time(NULL));
+    app.initApp();      
+    app.getRoot()->startRendering();
+  }
+  catch (Ogre::Exception& e) {
+    Ogre::LogManager::getSingleton().logMessage("An exception has occured: " + e.getFullDescription() + "\n");
+  }
+  app.closeApp();
+  return 0;
+}
