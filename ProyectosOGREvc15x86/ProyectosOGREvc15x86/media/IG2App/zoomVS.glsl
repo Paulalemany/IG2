32b75e3e594bb36272edd7119dd8f00b54964b92
Paulalemany <palemany@ucm.es>
2024-11-28 12:44:40 +0100

    El zoom realmente funciona
    falta que vaya cambiando

--- ProyectosOGREvc15x86/ProyectosOGREvc15x86/media/IG2App/zoomVS.glsl
+++ ProyectosOGREvc15x86/ProyectosOGREvc15x86/media/IG2App/zoomVS.glsl
@@ -6,6 +6,12 @@ uniform mat4 modelViewProjMat;
 out vec2 vUv0;
 
 void main(){
-    vUv0 = uv0;
+    //Diria que va por aqui arriba las cosas de zoom
+
+    //El 2 es el ZF para conseguir el efecto necesitamos que varie mientras pasa el tiempo
+    vUv0.s = (uv0.s - 0.5) * (2) + 0.5;
+    vUv0.t = (uv0.t - 0.5) * (2) + 0.5;
+
+    //vUv0 = uv0;
     gl_Position = modelViewProjMat * vertex;
 }
