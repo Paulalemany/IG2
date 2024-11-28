4cdbb2ca4c35d764bec36878a54e2010d9588e51
pokoli <psierr01@ucm.es>
2024-11-28 13:13:45 +0100

    duracion de la anim correcta

--- ProyectosOGREvc15x86/ProyectosOGREvc15x86/IG2App/IntroScene.cpp
+++ ProyectosOGREvc15x86/ProyectosOGREvc15x86/IG2App/IntroScene.cpp
@@ -105,12 +105,22 @@ void IntroScene::configAnims()
 
     // Variables
     int movementLength = 50;
-    Ogre::Real duration = 21.0;
     Vector3 keyframePos(0, 0, 0);
-    Ogre::Real durStep = duration / 10.0;
     Quaternion keyframeRot(0, 0, 0, 0);
 
-    animation = sm->createAnimation("sinbadMoving", duration);
+    Ogre::Real durStep[9]; // 9 keyframes
+
+    durStep[0] = 3;    // duracion kf 0
+    durStep[1] = 1 + durStep[0];      // kf1 + acumulado
+    durStep[2] = 3 + durStep[1];      // kf2 + acumulado
+    durStep[3] = 1 + durStep[2];      // kf3 + acumulado
+    durStep[4] = 3 + durStep[3];      // kf4 + acumulado
+    durStep[5] = 3 + durStep[4];      // kf5 + acumulado
+    durStep[6] = 1 + durStep[5];      // kf6 + acumulado
+    durStep[7] = 3 + durStep[6];      // kf7 + acumulado
+    durStep[8] = 3 + durStep[7];    // kf8 + acumulado -> suma 21 que seria la duracion total
+
+    animation = sm->createAnimation("sinbadMoving", durStep[8]); // importante la duracion total!!
     animation->setInterpolationMode(Ogre::Animation::IM_SPLINE);
     NodeAnimationTrack* track = animation->createNodeTrack(0);
     track->setAssociatedNode(sinbadNode);
@@ -119,81 +129,77 @@ void IntroScene::configAnims()
     TransformKeyFrame* kf;
 
     // Keyframe 0 (Init state)
-    kf = track->createNodeKeyFrame(durStep * 0);
+    kf = track->createNodeKeyFrame(0);
     keyframeRot = Quaternion(Degree(0), Vector3(0, 1, 0));
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    // Keyframe 1 GIRA DCH
-    kf = track->createNodeKeyFrame(durStep * 1);
+    // Keyframe 1 Quieto
+    kf = track->createNodeKeyFrame(durStep[0]);
+    keyframeRot = Quaternion(Degree(0), Vector3(0, 1, 0));
+
+    kf->setRotation(keyframeRot);
+    kf->setTranslate(keyframePos);
+
+    // Keyframe 2: Giro a la derecha
+    kf = track->createNodeKeyFrame(durStep[1]);
     keyframeRot = Quaternion(Degree(90), Vector3(0, 1, 0));
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    // Keyframe 2: Go to the right
-    kf = track->createNodeKeyFrame(durStep * 2);
+    // Keyframe 3 Movimiento a la derecha
+    kf = track->createNodeKeyFrame(durStep[2]);
     keyframePos += Ogre::Vector3::UNIT_X * movementLength;
     keyframeRot = Quaternion(Degree(90), Vector3(0, 1, 0));
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    // Keyframe 3 Stop
-    kf = track->createNodeKeyFrame(durStep * 3);
-    keyframeRot = Quaternion(Degree(90), Vector3(0, 1, 0));
-
-    kf->setRotation(keyframeRot);
-    kf->setTranslate(keyframePos);
-
-    //  Keyframe 4 GIRA IZQ
-    kf = track->createNodeKeyFrame(durStep * 4);
+    //  Keyframe 4 Giro a la izquierda
+    kf = track->createNodeKeyFrame(durStep[3]);
     keyframeRot = Quaternion(Degree(-90), Vector3(0, 1, 0));
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    // Keyframe 5: Vuelve
-    kf = track->createNodeKeyFrame(durStep * 5);
+    // Keyframe 5: Movimiento a la izquierda
+    kf = track->createNodeKeyFrame(durStep[4]);
     keyframePos += Ogre::Vector3::NEGATIVE_UNIT_X * movementLength;
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    // Keyframe 6: Sigue volviendo
-    kf = track->createNodeKeyFrame(durStep * 6);
+    // Keyframe 6: + Movimiento a la izquierda
+    kf = track->createNodeKeyFrame(durStep[5]);
     keyframePos += Ogre::Vector3::NEGATIVE_UNIT_X * movementLength;
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    //  Keyframe 7 Stop
-    kf = track->createNodeKeyFrame(durStep * 7);
-
-    kf->setRotation(keyframeRot);
-    kf->setTranslate(keyframePos);
-
-    //  Keyframe 8 GIRA DCH
-    kf = track->createNodeKeyFrame(durStep * 8);
+    //  Keyframe 7 Giro a la derecha
+    kf = track->createNodeKeyFrame(durStep[6]);
     keyframeRot = Quaternion(Degree(90), Vector3(0, 1, 0));
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    // Keyframe 9: Go to the right
-    kf = track->createNodeKeyFrame(durStep * 9);
+    // Keyframe 8: Movimiento a la derecha
+    kf = track->createNodeKeyFrame(durStep[7]);
     keyframePos += Ogre::Vector3::UNIT_X * movementLength;
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
 
-    //  Keyframe 10 Initial pos
-    kf = track->createNodeKeyFrame(durStep * 10);
+    //  Keyframe 9 Giro al frente para repetir animacion
+    kf = track->createNodeKeyFrame(durStep[8]);
     keyframeRot = Quaternion(Degree(0), Vector3(0, 1, 0));
 
     kf->setRotation(keyframeRot);
     kf->setTranslate(keyframePos);
+
+
 
     // Our defined animation
     animationState = sm->createAnimationState("sinbadMoving");
@@ -203,7 +209,6 @@ void IntroScene::configAnims()
     // aparte
     animationStateRunTop->setEnabled(false);
     animationStateRunBase->setEnabled(false);
-
     animationStateDance->setEnabled(true); // empieza bailando
 
     isDancing = false;
